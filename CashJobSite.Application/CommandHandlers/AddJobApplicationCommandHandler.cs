b6c488c42d13d52b97ea55a702cf540665e539e7
Alex Brown <AlexBrown@auctiontechnologygroup.com>
2017-04-11 12:37:50 +0100

    removed unused email service

--- CashJobSite.Application/CommandHandlers/AddJobApplicationCommandHandler.cs
+++ CashJobSite.Application/CommandHandlers/AddJobApplicationCommandHandler.cs
@@ -3,7 +3,6 @@ using CashJobSite.Application.Commands;
 using CashJobSite.Application.Logging;
 using CashJobSite.Application.Notifications;
 using CashJobSite.Application.Queries;
-using CashJobSite.Application.Services;
 using CashJobSite.Data;
 using CashJobSite.Models;
 using MediatR;
@@ -14,14 +13,12 @@ namespace CashJobSite.Application.CommandHandlers
     {
         private readonly IMediator _mediator;
         private readonly ICashJobSiteDbContext _dbContext;
-        private readonly IEmailService _emailService;
         private readonly ILogger _logger;
 
-        public AddJobApplicationCommandHandler(IMediator mediator, ICashJobSiteDbContext dbContext, IEmailService emailService, ILogger logger)
+        public AddJobApplicationCommandHandler(IMediator mediator, ICashJobSiteDbContext dbContext, ILogger logger)
         {
             _mediator = mediator;
             _dbContext = dbContext;
-            _emailService = emailService;
             _logger = logger;
         }
 
