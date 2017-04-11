dd98bdfe8510089a97544485891f0964ef8d061d
Alex Brown <AlexBrown@auctiontechnologygroup.com>
2017-04-11 14:59:30 +0100

    updated notifications

--- CashJobSite.Application/NotificationHandlers/JobApplicationSent/SendEmailToApplicant.cs
+++ CashJobSite.Application/NotificationHandlers/JobApplicationSent/SendEmailToApplicant.cs
@@ -1,13 +1,24 @@
 using CashJobSite.Application.Notifications;
+using CashJobSite.Application.Services;
 using MediatR;
 
 namespace CashJobSite.Application.NotificationHandlers.JobApplicationSent
 {
     public class SendEmailToApplicant : INotificationHandler<JobApplicationSentNotification>
     {
+        private readonly IEmailService _emailService;
+
+        public SendEmailToApplicant(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
         public void Handle(JobApplicationSentNotification notification)
         {
+            var emailSubject = "Job application sent.";
+            var emailBody = "You have sent a application for your job #" + notification.Job.Id;
 
+            _emailService.SendEmail(notification.CandidateEmail, emailSubject, emailBody);
         }
     }
 }
