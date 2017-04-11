9d12aa793f5f85f780b56459e216c009a8db6c8a
Alex Brown <AlexBrown@auctiontechnologygroup.com>
2017-04-11 15:00:35 +0100

    updated email notifications

--- CashJobSite.Application/NotificationHandlers/JobApplicationSent/SendEmailToApplicant.cs
+++ CashJobSite.Application/NotificationHandlers/JobApplicationSent/SendEmailToApplicant.cs
@@ -6,9 +6,19 @@ namespace CashJobSite.Application.NotificationHandlers.JobApplicationSent
 {
     public class SendEmailToApplicant : INotificationHandler<JobApplicationSentNotification>
     {
+        private readonly IEmailService _emailService;
+
+        public SendEmailToApplicant(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
         public void Handle(JobApplicationSentNotification notification)
         {
+            var emailSubject = "Job application sent.";
+            var emailBody = "You have sent a application for your job #" + notification.Job.Id;
 
+            _emailService.SendEmail(notification.CandidateEmail, emailSubject, emailBody);
         }
     }
 }
--- CashJobSite.Application/NotificationHandlers/JobApplicationSent/SendEmailToBoss.cs
+++ CashJobSite.Application/NotificationHandlers/JobApplicationSent/SendEmailToBoss.cs
@@ -6,9 +6,22 @@ namespace CashJobSite.Application.NotificationHandlers.JobApplicationSent
 {
     public class SendEmailToBoss : INotificationHandler<JobApplicationSentNotification>
     {
+        private readonly IEmailService _emailService;
+
+        public SendEmailToBoss(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
         public void Handle(JobApplicationSentNotification notification)
         {
+            var emailSubject = "Job application received.";
+            var emailBody = "You have a new application for your job #" + notification.Job.Id + "\n" +
+                            "Name: " + notification.CandidateName + "\n" +
+                            "Email: " + notification.CandidateEmail + "\n" +
+                            "Info: " + notification.CandidateInfo + "\n";
 
+            _emailService.SendEmail(notification.Job.BossEmail, emailSubject, emailBody);
         }
     }
 }
